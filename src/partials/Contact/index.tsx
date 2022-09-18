5a770c7a16276173ca73839c156d22d81243e534
Giulio Collesei <giuliocollesei@gmail.com>
2022-09-18 10:26:29 +0200

    WIP: Fixed Container props in Contact page
    undefined

--- src/partials/Contact/index.tsx
+++ src/partials/Contact/index.tsx
@@ -2,19 +2,21 @@ import style from './index.module.css'
 
 // Components
 import Section from 'components/Section'
-import Container from 'components/Container'
+import Container, { Row } from 'components/Container'
 import ContentBlock from 'components/ContentBlock'
 
 function Contact() {
   return (
     <Section name="contact" className={style.root}>
-      <Container right>
-        <ContentBlock>
-          <p>
-            Get in touch to find out more about digital experiences to effectively reach and engage
-            customers and target audiences.
-          </p>
-        </ContentBlock>
+      <Container grid>
+        <Row start={3} end={1}>
+          <ContentBlock>
+            <p>
+              Get in touch to find out more about digital experiences to effectively reach and
+              engage customers and target audiences.
+            </p>
+          </ContentBlock>
+        </Row>
       </Container>
     </Section>
   )
